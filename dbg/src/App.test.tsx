e6e29495f4f96984ee62a339aabb2fa78b697ca8
Uranus28 <yurasannikovv@gmail.com>
2024-06-02 22:01:19 +0500

    fixed loader

--- dbg/src/App.test.tsx
+++ dbg/src/App.test.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "./app/App";
+
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
