f2b746ee20c91cdc890a67f3e8fb580b28d5784f
Uranus28 <yurasannikovv@gmail.com>
2024-06-02 22:33:02 +0500

    some size adjustments

--- dbg/src/pages/PersonPage/PersonPage.tsx
+++ dbg/src/pages/PersonPage/PersonPage.tsx
@@ -2,18 +2,20 @@ import { FC } from "react";
 import { Button } from "../../entities/Button/Button";
 import { Loader } from "../../entities/Loader/Loader";
 import { IconType } from "../../shared/types/iconTypes";
-import style from "./personPage.module.scss";
+import styles from "./personPage.module.scss";
 import { Container } from "../../shared/Container/Container";
 
 export const PersonPage: FC = () => {
   return (
     <Container>
-      {/* <div>
+      <div className={styles.box}>
+        {/* <div>
         <Loader />
       </div> */}
-      <Button text="Refresh" icon={IconType.Refresh} />
-      <Button text="Save" icon={IconType.Date} />
-      <Button text="Search" icon={IconType.Search} />
+        <Button text="Refresh" icon={IconType.Refresh} />
+        <Button text="Save" icon={IconType.Date} />
+        <Button text="Search" icon={IconType.Search} />
+      </div>
     </Container>
   );
 };
