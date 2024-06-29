b6d8420c99361fb4f34591820c80d9efb7a17ce1
Uranus28 <yurasannikovv@gmail.com>
2024-06-29 12:08:46 +0500

    edit button

--- dbg/src/widgets/Profile/Profile.tsx
+++ dbg/src/widgets/Profile/Profile.tsx
@@ -6,6 +6,16 @@ export const Profile: FC = () => {
     <div className={styles.profile}>
       <p className={styles.profileText}>{"PROFILE"}</p>
       <p className={styles.boldName}>{"Name Surname"}</p>
+      <br />
+      <br />
+      <p id="info">
+        Login:
+        <span className={styles.regularText}>@namePerson</span>
+      </p>
+      <p id="info">
+        Password:
+        <span className={styles.linkText}>change password</span>
+      </p>
     </div>
   );
 };
